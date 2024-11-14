f9839de8b57827495bb7d3096990f35c93602c6c
rajkonkret <rajkonkret660@gmail.com>
2024-11-14 13:56:17 +0100

    [2:11] update warunki.py

--- day_2/warunki.py
+++ day_2/warunki.py
@@ -43,17 +43,53 @@ else:  # w przeciwnym przypadku
 # To nie jest Tomek
 # Indent Rainbow Plugin - pokazuje wyraźniej wcięcia i błedy wcięć
 
-podatek = 0
-zarobki = int(input("Podaj zarobki"))
-# pierwszy True oznacza wyjscie z calego drzewa
-if zarobki < 10_000:
-    podatek = 0
-elif zarobki < 30000:
-    podatek = 0.2
-elif zarobki < 100_000:
-    podatek = 0.4
-else:
-    podatek = 0.9
+# podatek = 0
+# zarobki = int(input("Podaj zarobki"))
+# # pierwszy True oznacza wyjscie z calego drzewa
+# if zarobki < 10_000:
+#     podatek = 0
+# elif zarobki < 30000:
+#     podatek = 0.2
+# elif zarobki < 100_000:
+#     podatek = 0.4
+# else:
+#     podatek = 0.9
+#
+# print(f"podatek wynosi {podatek * zarobki}")
+# # od 10_000 do 29_999 podatek 0.2
 
-print(f"podatek wynosi {podatek * zarobki}")
-# od 10_000 do 29_999 podatek 0.2
+suma_zam = 150
+if suma_zam > 100:
+    rabacik = 25
+else:
+    rabacik = 0
+
+print(f"Rabat wynosi {rabacik}")
+# Rabat wynosi 25
+
+rabat = 25 if suma_zam > 100 else 0
+print(f"Rabat wynosi {rabat}")
+# Rabat wynosi 25
+# Rabat wynosi 25
+
+# alert_system = "console"
+alert_system = "email"
+error = "error"
+lista_b = []  # pusta lista
+
+if alert_system == "console":
+    print("Stało się coś strasznego")
+elif alert_system == "email":
+    print("System email")
+    if error == "error":
+        lista_b.append("Krytyczny")
+    elif error == "medium":
+        lista_b.append('Ostrzeżenie')
+    else:
+        lista_b.append("Inny")
+else:
+    print("Nie znam takiego systemu")
+
+print(lista_b)
+# System email
+# ['Krytyczny']
